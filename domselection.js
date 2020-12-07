5ab7a6ac2a498bdb574412056032123b9e3ef48f
Andi_k <75620540+Timikaro@users.noreply.github.com>
2020-12-07 20:08:09 +0700

    Add files via upload

---
+++ domselection.js
@@ -0,0 +1,55 @@
+          // // document.getElementById
+
+// const nggojudul = document.getElementById('judul');
+// nggojudul.style.backgroundColor = 'magenta';
+// nggojudul.innerHTML ='INSTARGRAM';
+
+   
+
+
+        // // documents.getElemnetsByTagName()
+
+// const nggop = document.getElementsByTagName('p');
+// for (let i = 0; i < nggop.length; i++){
+//     nggop[i].style.backgroundColor = 'green';
+// }
+
+
+   
+
+
+        // // documents.getElemnetsByClassName
+
+// const nggop1 = document.getElementsByClassName('p1');
+// nggop1[0].innerHTML= 'ini diubah dari javascript';
+
+
+
+
+
+            // document.querySelector()
+
+
+// const nggop4 = document.querySelector('#b p');
+// nggop4.style.color = 'red';
+
+// const li = document.querySelector('section#b ul li:nth-child(2)');
+// li.style.backgroundColor = 'orange';
+// li.style.color = 'white';
+
+
+
+
+
+          //document.querySelectorAll()
+
+
+// const  p = document.querySelectorAll('p');
+// for (i = 0; i < p.length; i++){
+//     p[i].style.backgroundColor = 'green';
+// }
+
+
+
+
+
